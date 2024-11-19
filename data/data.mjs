141096bf35a2c9671c26e2c6633ba187ce6b8ad8
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-11-19 08:15:29 -0800

    added test data

---
+++ data/data.mjs
@@ -0,0 +1,53 @@
+const talents = [
+    {
+        name: "Charlotte Johnson",
+        email: "charlotte@testdata.com",
+        about: "A voice talent specializing in delivering personal messages and audiobook narrations.",
+        voice_sample: "s3://voice-cloning-zero-shot/a59cb96d-bba8-4e24-81f2-e60b888a0275/charlottenarrativesaad/manifest.json",
+        projects: [],
+        gender: "female",
+        languages: [{
+            language: "english",
+            accent: "canadian",
+        }]        
+    },
+    {
+        name: "David Adolfo",
+        email: "david@testdata.com",
+        about: "A voice talent specializing in audiobooks and translations.",
+        voice_sample: "s3://voice-cloning-zero-shot/d82d246c-148b-457f-9668-37b789520891/adolfosaad/manifest.json",
+        projects: [],
+        gender: "male",
+        languages: [{
+            language: "english",
+            accent: "american",
+        }]
+    },
+    {
+        name: "Navya",
+        email: "navya@testdata.com",
+        about: "A voice talent specializing in audiobooks.",
+        voice_sample: "s3://voice-cloning-zero-shot/e5df2eb3-5153-40fa-9f6e-6e27bbb7a38e/original/manifest.json",
+        projects: [],
+        gender: "female",
+        languages: [{
+            language: "english",
+            accent: "indian",
+        }]
+        
+    },
+    {
+        name: "Arthur",
+        email: "arthur@testdata.com",
+        about: "A voice talent specializing in podcasts and audiobooks.",
+        voice_sample: "s3://voice-cloning-zero-shot/38a41ac2-f574-421c-adb9-ce1bcb6f4a84/arthurmeditationsaad/manifest.json",
+        projects: [],
+        gender: "male",
+        languages: [{
+            language: "english",
+            accent: "british",
+        }]
+    },
+]
+
+export default talents
